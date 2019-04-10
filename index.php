1180ce410fe286a586dd815cfe15109d5ce28350
EstebanDEV <esteban.laisney.pro@gmail.com>
2019-04-10 11:48:16 +0200

    script connexion

--- index.php
+++ index.php
@@ -5,16 +5,37 @@
 </head>
 
 <body>
+<?php
+if(!empty($_GET['do'])) {
+    if ($_GET['do'] == "connect") {
+        if (empty($_POST['login']) || empty($_POST['password'])) {
+            echo 'Identifiants incorrects';
+        } else {
+            $q = $bdd->prepare('SELECT * FROM utilisateur WHERE util_login = :login AND util_password = :password');
+            $q->execute(array('login' => $_POST['login'], 'password' => $_POST['password']));
+
+            $result = $q->fetchAll();
+            
+            if (count($result) == 0) {
+                echo "erreur";
+            } else {
+                echo "réussi";
+            }
+        }      
+    }
+}
+
+?>
     <div class="background-rotate"></div>
     <div class="text-center d-flex h-100">
-        <form class="form w-25 m-auto align-item-center">
+        <form class="form w-25 m-auto align-item-center" action="?do=connect" method="post">
             <img class="mb-4" src="assets/img/Ticket.png" alt="" width="72" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Connexion à Ticket.IO</h1>
             <div class="form-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Adresse e-mail" required autofocus>
+                <input type="text" name="login" id="inputEmail" class="form-control" placeholder="Adresse e-mail" required autofocus>
             </div>
             <div class="form-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
             </div>
             
             <div class="form-group text-center">
